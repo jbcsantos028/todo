d5d321174fcb64a5334b65f8f1cd90b6b08b4005
jbcsantos028 <jbcsantos028@gmail.com>
2022-10-20 15:22:39 +0800

    finished task model, controller and view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def show
-    @task = Task.find(params[:id])
   end
 
   def index
@@ -12,11 +13,10 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def create
-    @task = Task.new(params.require(:task).permit(:name, :detail, :due_date))
+    @task = Task.new(task_params)
     if @task.save
       flash[:notice] = "Task was created successfully."
       redirect_to task_path(@task)
@@ -26,12 +26,27 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task = Task.find(params[:id])
-    if @task.update(params.require(:task).permit(:name, :detail, :due_date))
+    if @task.update(task_params)
       flash[:notice] = "Task was updated successfully."
       redirect_to task_path(@task)
     else
       render :edit
     end
   end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :detail, :due_date)
+  end
+
 end
