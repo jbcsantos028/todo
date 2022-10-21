edf34f81968cae64cde2e1d41c518520152266f4
jbcsantos028 <jbcsantos028@gmail.com>
2022-10-21 15:37:51 +0800

    add one-to-many association between user and tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :tasks
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
                     uniqueness: { case_sensitive: false }, 
