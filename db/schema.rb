0ee8be0636635a513ffc1be6d0220bc2a6de640a
jbcsantos028 <jbcsantos028@gmail.com>
2022-10-22 11:52:24 +0800

    add users create action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_21_071919) do
+ActiveRecord::Schema.define(version: 2022_10_21_082253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_10_21_071919) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
